9d644da52bfeaac8902cc0fa8935eb0d61c6f7f1
nyk <nykcasseano13@gmail.com>
2023-06-12 08:39:21 -0300

    último commit

--- fuction_outpust.py
+++ fuction_outpust.py
@@ -1,8 +1,11 @@
 #Function with Outputs
 
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "you didn't provide valid inputs"
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
-    return f"{formated_f_name} {formated_l_name}"
+    return f"Result: {formated_f_name} {formated_l_name}"
 
-print(format_name("Nyk", "Andrade"))
+print(format_name(input ("Nyk", "Andrade"))),
+input("what is your last name?")
