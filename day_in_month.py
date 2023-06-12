91f96901aaa92bd9274ca2825f28e4a0df5834a6
nyk <nykcasseano13@gmail.com>
2023-06-12 10:08:07 -0300

    último commit

---
+++ day_in_month.py
@@ -0,0 +1,12 @@
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print("Leap year")
+            else:
+                print("Not Leap year")
+        else:
+            print( "Leap year")
+    else:
+        print("Not Leap year")
+        
